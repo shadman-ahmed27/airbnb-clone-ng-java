bc0c0829ba6af2e1de48174e53570899d8a985a7
Shadman Ahmed <Shadifmg@gmail.com>
2025-01-21 23:11:04 -0600

    feat: imported the faUsers icon and made the avatar component

--- client/src/app/shared/font-awesome-icons.ts
+++ client/src/app/shared/font-awesome-icons.ts
@@ -1,5 +1,5 @@
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { faAirbnb } from '@fortawesome/free-brands-svg-icons';
+import { faUsers } from '@fortawesome/free-solid-svg-icons';
 
-
-export const fontAwesomeIcons: IconDefinition[] = [faAirbnb];
+export const fontAwesomeIcons: IconDefinition[] = [faAirbnb, faUsers];
