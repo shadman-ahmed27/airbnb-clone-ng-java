bfe31183969e091f90221075cc9271ca0ed6673e
Shadman Ahmed <Shadifmg@gmail.com>
2025-01-21 21:24:16 -0600

    feat: added shared folder in app for all the font awesome icons to make the project cleaner

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+
 import { RouterOutlet } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import {
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from './shared/font-awesome-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, FontAwesomeModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+  faIconLibrary: FaIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
---
+++ client/src/app/shared/font-awesome-icons.ts
@@ -0,0 +1,4 @@
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+import { faAirbnb } from '@fortawesome/free-brands-svg-icons';
+
+export const fontAwesomeIcons: IconDefinition[] = [faAirbnb];
