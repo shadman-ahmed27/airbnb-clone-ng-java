a684779b058799d94f9412c942aa58d344c7579a
Shadman Ahmed <Shadifmg@gmail.com>
2025-01-21 23:59:52 -0600

    feat: created the category component and added styles. category component pulls from category services and model, injects the service and persists state. displays the categories by technical name as long as its not ALL. being able to click and get diff categories

--- client/src/app/layout/navbar/category/category.component.ts
+++ client/src/app/layout/navbar/category/category.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CategoryService } from './category.service';
+import { Category } from './category.model';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './category.component.html',
-  styleUrl: './category.component.scss'
+  styleUrl: './category.component.scss',
 })
 export class CategoryComponent {
+isHome: any;
+onChangeCategory(_t2: Category) {
+throw new Error('Method not implemented.');
+}
+  categoryService: CategoryService = inject(CategoryService);
 
+  categories: Category[] | undefined;
+
+  currentActiveCategory = this.categoryService.getCategoryByDefault();
+
+  ngOnInit(): void {
+    this.fetchCategories();
+  }
+
+  private fetchCategories(): void {
+    this.categories = this.categoryService.getCategories();
+  }
 }
